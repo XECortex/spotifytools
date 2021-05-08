ef740b212328cbb18bae07b79f3c665346ea6d88
XECortex <57626963+XECortex@users.noreply.github.com>
2021-05-08 23:10:25 +0159

    Fixed colored output in install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]; then
-    echo "\033[1;91mPlease run as root\033[0m"
+    echo -e "\033[1;91mPlease run as root\033[0m"
     exit
 fi
 
-echo "\033[1mInstalling dependencies...\033[0m"
+echo -e "\033[1mInstalling dependencies...\033[0m"
 python3 -m pip install pygobject watchdog requests configupdater lxml beautifulsoup4
 
-echo "\033[1mSeting execute permissions...\033[0m"
+echo -e "\033[1mSeting execute permissions...\033[0m"
 chmod +x spotifytools
 chmod +x spotifytools.desktop
 chmod +x src/main.py
 chmod +x uninstall.sh
 
-echo "\033[1mCopying files...\033[0m"
+echo -e "\033[1mCopying files...\033[0m"
 if [ ! -d /opt/spotifytools/ ]; then
     mkdir /opt/spotifytools/
 fi
@@ -25,4 +25,4 @@ cp -a src/. /opt/spotifytools/
 cp -a version /opt/spotifytools/
 cp -a uninstall.sh /opt/spotifytools/
 
-echo "\033[1mDone!\033[0m"
+echo -e "\033[1mDone!\033[0m"
